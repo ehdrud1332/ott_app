3cefe7fe54ae74c10d9649208a93abafc0a9ab57
CastalJaemin <ehdrud1332@gmail.com>
2020-09-26 02:30:03 +0900

    Updating Tabs

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {MaterialIcons, Ionicons, Feather, AntDesign} from '@expo/vector-icons';
+
 import Like from '../screens/Like';
 import Movies from "../screens/Movies";
 import Search from "../screens/Search";
@@ -7,13 +9,71 @@ import Profile from "../screens/Profile";
 
 const tabs = createBottomTabNavigator();
 
-const Tabs = () => (
-    <tabs.Navigator>
-        <tabs.Screen name="Movies" component={Movies} />
-        <tabs.Screen name="Like" component={Like} />
-        <tabs.Screen name="Search" component={Search} />
-        <tabs.Screen name="Profile" component={Profile} />
-    </tabs.Navigator>
-)
+const getHeaderName = route =>
+    route?.state?.routeNames[route.state.index] || "Movies";
 
-export default Tabs;
+export default ({navigation, route}) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: getHeaderName(route)
+        });
+    }, [route]);
+
+    return (
+        <tabs.Navigator>
+            <tabs.Screen
+                name="Movies"
+                component={Movies}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <MaterialIcons
+                            name="local-movies"
+                            size={26}
+                            color={focused ? "black" : "grey"}
+                        />
+                    )
+                }}
+            />
+            <tabs.Screen
+                name="Like"
+                component={Like}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <AntDesign
+                            name="heart"
+                            size={26}
+                            color={focused ? "black" : "grey"}
+                        />
+                    )
+                }}
+            />
+            <tabs.Screen
+                name="Search"
+                component={Search}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Feather
+                            name="search"
+                            size={26}
+                            color={focused ? "black" : "grey"}
+                        />
+                    )
+                }}
+            />
+            <tabs.Screen
+                name="Profile"
+                component={Profile}options={{
+                tabBarIcon: ({focused}) => (
+                    <Ionicons
+                        name="md-person"
+                        size={26}
+                        color={focused ? "black" : "grey"}
+                    />
+                )
+            }}
+            />
+        </tabs.Navigator>
+    )
+}
+
